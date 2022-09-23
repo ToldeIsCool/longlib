e17d6f3649f46d8ec5df041fd53e5fc20c5dc270
ToldeIsCool <113589892+ToldeIsCool@users.noreply.github.com>
2022-09-23 20:17:20 +1000

    Update modernlonglibexample.lua

--- modernlonglibexample.lua
+++ modernlonglibexample.lua
@@ -1,38 +1,163 @@
--- // Tables
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/matas3535/PoopLibrary/main/Library.lua"))() -- Could Also Save It In Your Workspace And Do loadfile("Library.lua")()
--- // Variables
--- // Init
-local Window = Library:New({Name = "Splix Private Poop UI", Accent = Color3.fromRGB(25, 240, 100)})
---
-local Aimbot = Window:Page({Name = "Aimbot"})
-local Visuals = Window:Page({Name = "Visuals"})
-local Settings = Window:Page({Name = "Settings"})
---
-local Aimbot_Main = Aimbot:Section({Name = "Main", Side = "Left"})
-local Aimbot_Extra = Aimbot:Section({Name = "Extra", Side = "Right"})
---
-local Visuals_Enemies, Visuals_Teamates, Visuals_Self = Visuals:MultiSection({Sections = {"Enemies", "Teamates", "Self"}, Side = "Left", Size = 200})
---
-local Settings_Main = Settings:Section({Name = "Main", Side = "Left"})
--- // Aimbot
-Aimbot_Main:Toggle({Name = "Enabled", Default = false, Pointer = "AimbotMain_Enabled"})
-:Keybind({Default = Enum.KeyCode.E, KeybindName = "Aimbot", Mode = "Hold", Pointer = "AimbotMain_Bind"})
-Aimbot_Main:Slider({Name = "Smoothness", Minimum = 1, Maximum = 30, Default = 1.5, Decimals = 0.1, Pointer = "AimbotMain_Smoothness"})
-Aimbot_Main:Multibox({Name = "Hit-Part", Minimum = 1, Options = {"Head", "Torso", "Arms", "Legs"}, Default = {"Head", "Torso"}, Pointer = "AimbotMain_Hitpart"})
---
-Aimbot_Extra:Keybind({Name = "Readjustment", Default = Enum.UserInputType.MouseButton2, KeybindName = "Readjustment", Mode = "Hold", Pointer = "AimbotExtra_Readjustment"})
-Aimbot_Extra:Dropdown({Name = "Aimbot-Type", Options = {"Relative", "Absolute", "Camera", "Camera Relative"}, Default = "Relative", Pointer = "AimbotExtra_Type"})
-Aimbot_Extra:Colorpicker({Name = "Locking-Color", Info = "Aimbot Locked Color", Alpha = 0.5, Default = Color3.fromRGB(255, 0, 0), Pointer = "AimbotExtra_Color"})
--- // Visuals
-local VisualsEnemies_BoxEsp = Visuals_Enemies:Toggle({Name = "Box-Esp", Default = false, Pointer = "VisualsEnemies_BoxEsp"})
-VisualsEnemies_BoxEsp:Colorpicker({Info = "ESP-Box Color", Alpha = 0.75, Default = Color3.fromRGB(200, 200, 200), pointer = "VisualsEnemies_BoxColor"})
-VisualsEnemies_BoxEsp:Colorpicker({Info = "ESP-Box Visible Color", Alpha = 0.25, Default = Color3.fromRGB(200, 100, 100), pointer = "VisualsEnemies_BoxVisibleColor"})
---
-Visuals_Self:Toggle({Name = "Enabled", Default = true, Pointer = "VisualsSelf_Enabled"})
--- // Settings
-Settings_Main:ConfigBox({})
-Settings_Main:ButtonHolder({Buttons = {{"Load", function() end}, {"Save", function() end}}})
-Settings_Main:Label({Name = "Unloading will fully unload\neverything, so save your\nconfig before unloading.", Middle = true})
-Settings_Main:Button({Name = "Unload", Callback = function() Window:Unload() end})
--- // Initialisation
-Window:Initialize()
+local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/ToldeIsCool/longlib/main/modernlonglib.lua'))()
+local Flags = Library.Flags
+
+local Window = Library:Window({
+    Text = "Baseplate"
+})
+
+local Tab = Window:Tab({
+    Text = "Aiming"
+})
+
+local Tab2 = Window:Tab({
+    Text = "Visual"
+})
+
+local ChamsSection = Tab2:Section({
+    Text = "Chams"
+})
+
+local Section = Tab:Section({
+    Text = "Aimbot"
+})
+
+local Section2 = Tab:Section({
+    Text = "FOV"
+})
+
+local Section3 = Tab:Section({
+    Text = "Misc",
+    Side = "Right"
+})
+
+ChamsSection:Toggle({
+    Text = "Enabled"
+})
+
+ChamsSection:Toggle({
+    Text = "Color"
+})
+
+ChamsSection:Toggle({
+    Text = "Filled"
+})
+
+ChamsSection:Toggle({
+    Text = "Team Check"
+})
+
+Section:Toggle({
+    Text = "Enabled"
+})
+
+Section:Toggle({
+    Text = "Wall Check"
+})
+
+Section:Toggle({
+    Text = "Smooth Aimbot"
+})
+
+Section2:Toggle({
+    Text = "Enabled"
+})
+
+Section2:Toggle({
+    Text = "Filled FOV"
+})
+
+Section2:Toggle({
+    Text = "FOV Transparency",
+    Tooltip = "Changes your fov transparency."
+})
+
+Section2:Button({
+    Text = "Reset FOV",
+    Tooltip = "This resets your aimbot fov."
+})
+
+Section3:Toggle({
+    Text = "Infinite Ammo"
+})
+
+Section3:Toggle({
+    Text = "No Spread"
+})
+
+Section3:Toggle({
+    Text = "No Bullet Drop",
+    Default = true
+})
+
+Section3:Toggle({
+    Text = "Full Auto"
+})
+
+local a = Section3:Toggle({
+    Text = "No Recoil"
+})
+
+local label = Section3:Label({
+    Text = "This is a label.",
+    Color = Color3.fromRGB(217, 97, 99),
+    Tooltip = "This is a label."
+})
+
+local dropdown = Section:Dropdown({
+    Text = "Dropdown",
+    List = {"Head","Torso","Random"},
+    Flag = "Choosen",
+    Callback = function(v)
+        warn(v)
+    end
+})
+
+Section:RadioButton({
+    Text = "RadioButton",
+    Options = {"Legit","Blatant"},
+    Callback = function(v)
+        warn(v)
+    end
+})
+
+Section:Toggle({
+    Text = "Silent Aimbot"
+})
+
+Section:Input({
+    Placeholder = "Webhook URL",
+    Flag = "URL"
+})
+
+Section:Keybind({
+    Default = Enum.KeyCode.E,
+    Text = "Aimbot Key",
+    Callback = function()
+        warn("Pressed")
+    end
+})
+
+Section:Slider({
+    Text = "Slider Test",
+    Default = 5,
+    Minimum = 0,
+    Maximum = 50,
+    Flag = "SliderFlag",
+    Callback = function(v)
+        warn(v)
+    end
+})
+
+Tab:Select()
+
+wait(5)
+
+--[[dropdown:Refresh({
+    List = {"Head", "Feet"}
+})--]]
+
+label:Set({
+    Text = "This is a red label."
+})
+
+a:Set(true)
